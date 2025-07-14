345791941fce87d12d14e4564d03bb762d45d0cf
DEVAN RAMU <rdevan2003@gmail.com>
2025-07-14 12:11:32 +0530

    Add files via upload
    This is the second exercise of the spring rest in the week 4. Here I am loading the country name from the spring configuration XML.

--- week4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ week4/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,14 +1,22 @@
 package com.cognizant.spring_learn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-		System.out.println("✅ Spring Learn Application Started");
-		SpringApplication.run(SpringLearnApplication.class, args);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 
+    public static void main(String[] args) {
+        displayCountry();
+    }
+
+    public static void displayCountry() {
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("country.xml")) {
+            Country country = context.getBean("country", Country.class);
+            LOGGER.debug("Country : {}", country.toString()); // this uses LOGGER
+        }
+    }
 }
